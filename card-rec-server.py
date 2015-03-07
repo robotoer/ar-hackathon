d8ce61298622df26c5414037ccecf0e3ec537a5b
Milo <milo@wibidata.com>
2015-03-07 01:59:59 -0800

    Fix codestyle.

--- card-rec-server.py
+++ card-rec-server.py
@@ -4,11 +4,13 @@
 A flask webapp that does nothing but receive image attachements and returns
 card suit/rank.
 
+Usage information: python card-rec-server.py <training image file> <training labels file>
+
 API:
 
-  POST http://127.0.0.1:5000/ Attempts to post a file. The request must be 
+  POST http://127.0.0.1:5000/ Attempts to post a file. The request must be
 sent with a enctype of multipart/form-data and the file must be included as
-form-data keyed as `image`. The server will look for a card in the center of 
+form-data keyed as `image`. The server will look for a card in the center of
 the image, and return a json doc with fields for 'suit' and 'rank' if it finds
 one. If no card is found, these fields will not be present.
 
@@ -16,6 +18,7 @@ one. If no card is found, these fields will not be present.
 
 
 import sys
+
 import cv2
 from flask import abort, Flask, jsonify, request
 import numpy as np
@@ -33,13 +36,13 @@ training = None
 ###############################################################################
 def rectify(h):
     h = h.reshape((4,2))
-    hnew = np.zeros((4,2),dtype = np.float32)
+    hnew = np.zeros((4,2), dtype = np.float32)
 
     add = h.sum(1)
     hnew[0] = h[np.argmin(add)]
     hnew[2] = h[np.argmax(add)]
 
-    diff = np.diff(h,axis = 1)
+    diff = np.diff(h, axis = 1)
     hnew[1] = h[np.argmin(diff)]
     hnew[3] = h[np.argmax(diff)]
 
@@ -51,16 +54,16 @@ def rectify(h):
 ###############################################################################
 def preprocess(img):
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray,(5,5),2 )
-    thresh = cv2.adaptiveThreshold(blur,255,1,1,11,1)
+    blur = cv2.GaussianBlur(gray, (5,5), 2 )
+    thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 1)
     return thresh
 
 
-def imgdiff(img1,img2):
-    img1 = cv2.GaussianBlur(img1,(5,5),5)
-    img2 = cv2.GaussianBlur(img2,(5,5),5)
+def imgdiff(img1, img2):
+    img1 = cv2.GaussianBlur(img1, (5,5), 5)
+    img2 = cv2.GaussianBlur(img2, (5,5), 5)
     diff = cv2.absdiff(img1,img2)
-    diff = cv2.GaussianBlur(diff,(5,5),5)
+    diff = cv2.GaussianBlur(diff, (5,5), 5)
     flag, diff = cv2.threshold(diff, 200, 255, cv2.THRESH_BINARY)
     return np.sum(diff)
 
@@ -69,15 +72,16 @@ def find_closest_card(training, img):
     if img is None:
         return ["*", "*"]
     features1 = preprocess(img)
-    features2 = cv2.flip(cv2.transpose(features1),1)
-    return sorted(training.values(),
-        key = lambda x: min(imgdiff(x[1],features1), imgdiff(x[1],features2)))[0][0]
+    features2 = cv2.flip(cv2.transpose(features1), 1)
+    return sorted(
+        training.values(),
+        key = lambda x: min(imgdiff(x[1], features1), imgdiff(x[1], features2)))[0][0]
 
 
-def cross_sign(first,second,point):
+def cross_sign(first, second, point):
     a = list(first[0])
     b = list(second[0])
-    return (a[0]-b[0])*(point[1]-b[1]) - (a[1]-b[1])*(point[0]-b[0]) >= 0
+    return (a[0] - b[0]) * (point[1] - b[1]) - (a[1] - b[1]) * (point[0] - b[0]) >= 0
 
 
 def is_internal(point, contour):
@@ -95,27 +99,20 @@ def get_center_card(im, center):
 
     contours, hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
-    contours = sorted(contours, key=cv2.contourArea,reverse=True)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
     contours = contours[:min(len(contours), 10)]
 
     for card in contours:
-        peri = cv2.arcLength(card,True)
-        contour = cv2.approxPolyDP(card,0.02*peri,True)
-
-        #imx = im.copy()
-        #cv2.drawContours(imx,cv2.approxPolyDP(card,0.02*peri,True),-1,(255,255,0),6)
-        #cv2.circle(imx, center, 2, (0,0,255), -1)
-        #cv2.imshow('Contours',imx)
-        #raw_input("")
-        #cv2.destroyAllWindows()
+        peri = cv2.arcLength(card, True)
+        contour = cv2.approxPolyDP(card, 0.02*peri, True)
 
         if is_internal(center, contour):
             approx = rectify(cv2.approxPolyDP(card,0.02*peri,True)[:4])
 
-            h = np.array([ [0,0],[449,0],[449,449],[0,449] ],np.float32)
+            h = np.array([ [0,0], [449,0], [449,449], [0,449] ], np.float32)
 
-            transform = cv2.getPerspectiveTransform(approx,h)
-            warp = cv2.warpPerspective(im,transform,(450,450))
+            transform = cv2.getPerspectiveTransform(approx, h)
+            warp = cv2.warpPerspective(im, transform, (450,450))
 
             return warp
 
@@ -126,23 +123,23 @@ def get_center_card(im, center):
 # Card Extraction For Training
 ###############################################################################
 def get_cards(im, numcards=4, showcontours=False):
-    gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray,(1,1),1000)
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (1,1), 1000)
     flag, thresh = cv2.threshold(blur, 120, 255, cv2.THRESH_BINARY)
 
-    contours, hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    contours = sorted(contours, key=cv2.contourArea,reverse=True)[:numcards]
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:numcards]
 
     for card in contours:
-        peri = cv2.arcLength(card,True)
+        peri = cv2.arcLength(card, True)
 
-        approx = rectify(cv2.approxPolyDP(card,0.02*peri,True)[:4])
+        approx = rectify(cv2.approxPolyDP(card, 0.02*peri, True)[:4])
 
-        h = np.array([ [0,0],[449,0],[449,449],[0,449] ],np.float32)
+        h = np.array([ [0,0], [449,0], [449,449], [0,449] ], np.float32)
 
-        transform = cv2.getPerspectiveTransform(approx,h)
-        warp = cv2.warpPerspective(im,transform,(450,450))
+        transform = cv2.getPerspectiveTransform(approx, h)
+        warp = cv2.warpPerspective(im, transform, (450,450))
 
         yield warp
 
@@ -153,7 +150,7 @@ def get_training(training_labels_filename, training_image_filename, num_training
     labels = {}
     for line in file(training_labels_filename):
         key, num, suit = line.strip().split()
-        labels[int(key)] = (num,suit)
+        labels[int(key)] = (num, suit)
 
     print "Training"
 
